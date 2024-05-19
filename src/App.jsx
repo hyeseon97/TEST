a23fecd0478b91e8d284c8a11a37d8d5cb1b71fc
박헤선 <qkr8364@naver.com>
2024-05-20 02:24:02 +0900

    전

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
+import after from "../src/assets/after.png";
+import before from "../src/assets/before.png";
 
 function App() {
   return (
-    <div className={styles.App}>
-      <h1>헤헤헷</h1>
+    <div className={styles.App} style={{ width: '100%', height: '100vh' }}>
+      {/* <img src={after} style={{ width: '100%', height: '100%', objectFit: 'cover' }} /> */}
+      <img src={before} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
     </div>
   );
 }
