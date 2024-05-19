214df106240d31b9acc8cd06e0d27f755e2bdb32
박헤선 <qkr8364@naver.com>
2024-05-20 00:24:19 +0900

    FIRST COMMIT

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <h1>SSAFY 1년동안 수고하셨습니다 :)</h1>
+    </div>
+  );
+}
+
+export default App;
